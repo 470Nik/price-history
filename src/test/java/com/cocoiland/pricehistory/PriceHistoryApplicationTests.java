730f917b9aa15897d8c17f39df0e1fe12f278685
Nikhil <41849216+Nikgithubit@users.noreply.github.com>
2023-09-30 01:23:34 +0530

    First Commit

---
+++ src/test/java/com/cocoiland/pricehistory/PriceHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoiland.pricehistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
